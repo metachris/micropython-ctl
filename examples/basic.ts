2ae5bbf45da2a1833285b9186904e36bdb65b9ac
Chris Hager <chris@linuxuser.at>
2020-11-24 23:46:49 +0100

    cleanup

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,6 +1,26 @@
 import { WebREPL, WindowWithWebRepl, WebReplMode } from '../src/main'
 import { InvalidPassword } from '../src/errors'
-import WebSocket from 'ws'
+
+import readline from 'readline'
+
+let lastKey = null
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+  console.log(str)
+  console.log(key)
+
+  if (key.sequence === '\u0004' && key.ctrl) {
+    console.log('x')
+    if (lastKey.sequence === '\u001d') {
+      process.exit(1)
+    }
+  }
+})
+
+// import tty from 'tty'
+// //require('tty').setRawMode(true);
+// tty.
 
 const host = '10.12.50.101'
 const password = 'test'
@@ -10,10 +30,15 @@ const run = async () => {
     const webrepl = new WebREPL()
     await webrepl.connect(host, password)
     console.log('after connect')
-    const output = await webrepl.runReplCommand('import os; os.listdir()')
-    console.log('after run command', output)
-    await webrepl.close()
-    console.log('after close')
+    // const output = await webrepl.runReplCommand('import os; os.listdir()')
+    // console.log('after run command', output)
+
+    // for await (const line of rl) {
+    //   console.log(line)
+    // }
+
+    // await webrepl.close()
+    // console.log('after close')
 
   } catch (e) {
     // probably invalid password, but could also invalid host or another websocket error
