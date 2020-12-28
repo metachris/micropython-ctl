fe04221c606e9ac1e496e072d22b49a1d168868a
Chris Hager <chris@linuxuser.at>
2020-12-28 01:15:40 +0100

    dir layout

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist-browser'),
     library: 'MicroPythonCtl',
   },
 };
