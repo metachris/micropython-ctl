a5b82ee6764ba3429d13080deb9558bb500180b1
Chris Hager <chris@linuxuser.at>
2021-01-05 18:17:42 +0100

    terminal example cleanup

--- examples/terminal.ts
+++ examples/terminal.ts
@@ -1,7 +1,9 @@
+/**
+ * REPL terminal. Quit by pressing Ctrl+K
+ */
 import readline from 'readline'
 import { MicroPythonDevice } from '../src/main'
 
-// const HOST = '10.12.50.101'
 const HOST = '10.0.1.10'
 const PASSWORD = 'test'
 
@@ -13,47 +15,29 @@ micropython.onTerminalData = (data) => process.stdout.write(data)
 
 // Keystroke capture for interactive REPL
 const setupKeyboardCapture = () => {
-  let specialMode = false
   readline.emitKeypressEvents(process.stdin);
   process.stdin.setRawMode(true);
   process.stdin.on('keypress', async (str, key) => {
     // console.log(str, key)
 
-    // Enter special mode with ^] (force quit with ^] and ^D)
-    const wasSpecialMode = specialMode
-    specialMode = key.sequence === '\u001d'
-    if (wasSpecialMode && key.sequence === '\u0004' && key.ctrl) {
-      process.exit(0)
-    }
+    // Quit on Ctrl+K
+    if (key.name === 'k' && key.ctrl) process.exit(0)
 
-    if (!micropython.isConnected()) return
-
-    if (wasSpecialMode) {
-      if (key.sequence === 'w') {
-        console.log(micropython.getState())
-
-      } else if (key.sequence === 'l') {
-        const files = await micropython.listFiles()
-        console.log(`\nfiles:`, files)
-
-      } else if (key.sequence === 'p') {
-        const cmd = 'import os; os.listdir()'
-        micropython.sendData(cmd + '\r')
-      }
-
-      return
-    }
-
-    // Send key to webrepl
-    if (micropython.isTerminalMode()) {
+    // Send anything to the device, if connected
+    if (micropython.isConnected() && micropython.isTerminalMode()) {
       micropython.sendData(str)
     }
   });
 }
 
-// Connect to webrepl and do stuff
+// Connect to device
 (async () => {
   setupKeyboardCapture()
-  await micropython.connectNetwork(HOST, PASSWORD)
-  micropython.sendData('\x02')  // Ctrl+B: exit raw repl and show micropython header
+
+  // Connect over network or serial
+  // await micropython.connectNetwork(HOST, PASSWORD)
+  await micropython.connectSerial('/dev/tty.SLAB_USBtoUART')
+
+  // Send Ctrl+B (exit raw repl and show micropython header)
+  micropython.sendData('\x02')
 })()
